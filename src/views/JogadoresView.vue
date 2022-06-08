e8c0e21ce0c8c8f96765ec383b0c62037fe7a144
RuiDevin <rafaelalvaro00@gmail.com>
2022-06-08 09:26:48 -0300

    8-6-2022

---
+++ src/views/JogadoresView.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  data() {
+    return {
+      jogadores: [
+        { id: 1, name: "Jogador 1" },
+        { id: 2, name: "Jogador 2" },
+        { id: 3, name: "Jogador 3" },
+        { id: 4, name: "Jogador 4" },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <main id="main">BEM VINDO TIME</main>
+  <div class="container">
+    <h2>Gerenciamento de jogadores</h2>
+  </div>
+  <div class="form-input">
+    <input type="text" />
+    <button>Salvar</button>
+  </div>
+  {{ jogadores }}
+  <div class="list-times">
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Açoẽs</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="time in times" :key="time.id">
+          <td>1</td>
+          <td>Time 1</td>
+          <td>???</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/TimeView.vue
@@ -0,0 +1,48 @@
+<script>
+ export default {
+ data(){
+     return {
+       times: [
+         {id: 1, name: "Time 1"},
+         {id: 2, name: "Time 2"},
+         {id: 3, name: "Time 3"},
+         {id: 4, name: "Time 4"},
+         
+       ],
+     };
+   },
+ };
+</script>
+
+<template>
+  <main id="main">BEM VINDO TIME</main>
+  <div class = "container">
+  <h2>Gerenciamento de time</h2>
+  </div>
+  <div class="form-input">
+    <input type="text" placeholder="Nome">
+    <input type="text" placeholder="Time">
+    <button>Salvar</button>
+  </div>
+  {{times}} 
+  <div class="list-times">
+    <table>
+      <thead>
+      <tr>
+      <th>ID</th>
+      <th>Nome</th>
+      <th>Açoẽs</th>
+      </tr>
+      </thead>
+      <tbody>
+        <tr v-for="time in times" :key="time.id">
+        <td>{{time.id}}</td>
+        <td>{{time.name}}</td>
+        <td>???</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+
